726d3f3769ca8c6c1451e47464392b11ae508300
MartinMwiti <martinmwiti007@gmail.com>
2021-06-26 08:45:01 +0300

    Update

---
+++ Spring-boot-l1/src/main/java/com/shadowJava/Springboot/l1/SpringBootL1Application.java
@@ -0,0 +1,13 @@
+package com.shadowJava.Springboot.l1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootL1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootL1Application.class, args);
+	}
+
+}
---
+++ Spring-boot-l1/src/test/java/com/shadowJava/Springboot/l1/SpringBootL1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowJava.Springboot.l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootL1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
