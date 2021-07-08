e33ccd7cf0c1f34bfa1db3d9f30860adccf50fb8
MartinMwiti <martinmwiti007@gmail.com>
2021-07-08 21:38:02 +0300

    update

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        int age;
+        age = age + 7;
+        System.out.println( "My age is: " + age );
+    }//End of main
+  }//End of FirstJavaProgram Class
+
+
